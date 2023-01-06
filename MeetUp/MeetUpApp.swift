8803ddea546223f24fdcad499aa5ca6104fff87b
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-01-06 17:01:23 -0300

    Initial Commit

---
+++ MeetUp/MeetUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetUpApp.swift
+//  MeetUp
+//
+//  Created by Orlando Moraes Martins on 06/01/23.
+//
+
+import SwiftUI
+
+@main
+struct MeetUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
